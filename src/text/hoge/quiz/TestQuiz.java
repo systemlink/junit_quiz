7f49f69dccf763e4b7a0753307985b2d3e0dc869
taiichi.igawa <ihciiat@gmail.com>
2015-04-29 08:42:42 +0900

    テストコードの不備を修正

--- src/text/hoge/quiz/TestQuiz.java
+++ src/text/hoge/quiz/TestQuiz.java
@@ -23,15 +23,15 @@ public class TestQuiz {
 	@Test
 	public void test2() {
 		// 曜日 (例外処理）
-		Assert.assertEquals("日", DateUtils.toDayOfWeekForJapanese(1));
-		Assert.assertEquals("月", DateUtils.toDayOfWeekForJapanese(2));
-		Assert.assertEquals("火", DateUtils.toDayOfWeekForJapanese(3));
-		Assert.assertEquals("水", DateUtils.toDayOfWeekForJapanese(4));
-		Assert.assertEquals("木", DateUtils.toDayOfWeekForJapanese(5));
-		Assert.assertEquals("金", DateUtils.toDayOfWeekForJapanese(6));
-		Assert.assertEquals("土", DateUtils.toDayOfWeekForJapanese(7));
+		Assert.assertEquals("日", DateUtils.toDayOfWeekForJapaneseThrowsException(1));
+		Assert.assertEquals("月", DateUtils.toDayOfWeekForJapaneseThrowsException(2));
+		Assert.assertEquals("火", DateUtils.toDayOfWeekForJapaneseThrowsException(3));
+		Assert.assertEquals("水", DateUtils.toDayOfWeekForJapaneseThrowsException(4));
+		Assert.assertEquals("木", DateUtils.toDayOfWeekForJapaneseThrowsException(5));
+		Assert.assertEquals("金", DateUtils.toDayOfWeekForJapaneseThrowsException(6));
+		Assert.assertEquals("土", DateUtils.toDayOfWeekForJapaneseThrowsException(7));
 		try {
-			DateUtils.toDayOfWeekForJapanese(8);
+			DateUtils.toDayOfWeekForJapaneseThrowsException(8);
 			Assert.fail();
 		} catch (IllegalArgumentException e) {
 			Assert.assertEquals("引数に1-7の数字を指定してください。指定値=8", e.getMessage());
@@ -42,8 +42,7 @@ public class TestQuiz {
 	public void test3() {
 		// StringUtils.distinct()
 		Assert.assertEquals(null, StringUtils.distinct(null));
-		Assert.assertEquals(asList("blue", "red", "black"),
-				StringUtils.distinct(asList("blue", "red", "black", "blue")));
+		Assert.assertEquals(asList("blue", "red", "black"), StringUtils.distinct(asList("blue", "red", "black", "blue")));
 	}
 
 	@Test
@@ -67,5 +66,4 @@ public class TestQuiz {
 		Assert.assertFalse(ArrayUtils.contains(asList("1"), asList("1", "2", "3")));
 		Assert.assertTrue(ArrayUtils.contains(asList("1", "2", "3"), asList("1", "2", "3")));
 	}
-
 }
