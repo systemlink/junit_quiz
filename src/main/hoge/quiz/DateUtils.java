c08cbf8990d1d87a31b96e1b46750d09dbe53c62
yoshi836 <y-toya@sys-link.jp>
2015-04-29 17:06:32 +0900

    test1 added

--- src/main/hoge/quiz/DateUtils.java
+++ src/main/hoge/quiz/DateUtils.java
@@ -1,11 +1,37 @@
 package hoge.quiz;
 
 public class DateUtils {
-	
 	public static String toDayOfWeekForJapanese(int week){
-		return null;
+		String test = "";
+
+		switch(week){
+			case 0:
+				test = "";
+				break;
+			case 1:
+				test = "日";
+				break;
+			case 2:
+				test = "月";
+				break;
+			case 3:
+				test = "火";
+				break;
+			case 4:
+				test = "水";
+				break;
+			case 5:
+				test = "木";
+				break;
+			case 6:
+				test = "金";
+				break;
+			case 7:
+				test = "土";
+		}
+		return test;
 	}
-	
+
 	public static String toDayOfWeekForJapaneseThrowsException(int week) throws IllegalArgumentException{
 		return null;
 	}
