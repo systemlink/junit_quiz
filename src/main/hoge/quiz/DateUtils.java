10a5a4fee0aaaffebd441f462259b53663e0f6de
yoshi836 <y-toya@sys-link.jp>
2015-04-30 09:27:37 +0900

    test1 added

--- src/main/hoge/quiz/DateUtils.java
+++ src/main/hoge/quiz/DateUtils.java
@@ -1,35 +1,26 @@
 package hoge.quiz;
 
+import java.util.HashMap;
+
 public class DateUtils {
 	public static String toDayOfWeekForJapanese(int week){
-		String test = "";
 
-		switch(week){
-			case 0:
-				test = "";
-				break;
-			case 1:
-				test = "日";
-				break;
-			case 2:
-				test = "月";
-				break;
-			case 3:
-				test = "火";
-				break;
-			case 4:
-				test = "水";
-				break;
-			case 5:
-				test = "木";
-				break;
-			case 6:
-				test = "金";
-				break;
-			case 7:
-				test = "土";
+		HashMap<Integer,String> hashWeek = new HashMap<Integer,String>();
+
+		{
+			hashWeek.put(0, "");
+			hashWeek.put(1,"日");
+			hashWeek.put(2,"月");
+			hashWeek.put(3,"火");
+			hashWeek.put(4,"水");
+			hashWeek.put(5,"木");
+			hashWeek.put(6,"金");
+			hashWeek.put(7,"土");
+			hashWeek.put(8,"");
 		}
-		return test;
+
+		String toDayOfWeek = hashWeek.get(week);
+		return toDayOfWeek;
 	}
 
 	public static String toDayOfWeekForJapaneseThrowsException(int week) throws IllegalArgumentException{
