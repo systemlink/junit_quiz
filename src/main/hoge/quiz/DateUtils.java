14cbcee477e5b220202f8bde2392eb85dd0547ad
kiyotani masaki <makky0416@gmail.com>
2015-05-01 18:11:44 +0900

    test3実装しました。

--- src/main/hoge/quiz/DateUtils.java
+++ src/main/hoge/quiz/DateUtils.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class DateUtils {
 
 	static HashMap<Integer,String> map = new HashMap<Integer,String>();
-	static String[] array = {"","日","月","火","水","木","金","土"};
+	static String[] array = {"日","月","火","水","木","金","土"};
 
 	static{
 		for(int i=0;i<array.length;i++){
@@ -15,7 +15,7 @@ public class DateUtils {
 
 	public static String toDayOfWeekForJapanese(int week){
 
-		String weekName = map.get(week);
+		String weekName = map.get(week - 1);
 		if(weekName == null) weekName = "";
 
 		return weekName;
@@ -23,9 +23,9 @@ public class DateUtils {
 
 	public static String toDayOfWeekForJapaneseThrowsException(int week) throws IllegalArgumentException{
 
-		String weekName = map.get(week);
+		String weekName = map.get(week - 1);
 
-		if(weekName == null || weekName == ""){
+		if(weekName == null){
 			String msg = "引数に1-7の数字を指定してください。指定値=" + week;
 			throw new IllegalArgumentException(msg);
 		}
--- src/main/hoge/quiz/StringUtils.java
+++ src/main/hoge/quiz/StringUtils.java
@@ -1,9 +1,32 @@
 package hoge.quiz;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StringUtils {
+
 	public static List<String> distinct(List<String> list){
-		return null;
+		List<String> uniqueList = new ArrayList<String>();
+
+		if(list != null){
+			uniqueList.add(list.get(0));
+
+			for(int i=1; i<list.size(); i++){
+				int j;
+
+				for(j=0;j<uniqueList.size(); j++){
+					if(uniqueList.get(j).equals(list.get(i))){
+						break;
+					}
+				}
+				if(j == uniqueList.size()){
+					uniqueList.add(list.get(i));
+				}
+			}
+		}else{
+			uniqueList = list;
+		}
+
+		return uniqueList;
 	}
 }
