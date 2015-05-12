6b4677466aa86126f49c75564d6a3e39d12e3224
yoshi836 <y-toya@sys-link.jp>
2015-05-12 11:14:35 +0900

    test5 contains処理を実装しました。

--- src/main/hoge/quiz/ArrayUtils.java
+++ src/main/hoge/quiz/ArrayUtils.java
@@ -17,6 +17,16 @@ public class ArrayUtils {
 	}
 
 	public static boolean contains(List<String> list1, List<String> list2){
+
+		if(list1 == null || list2 == null){
+			return false;
+		}
+
+		if(list1.size()  >= list2.size()){
+			if(!list1.contains(list2)){
+				return true;
+			}
+		}
 		return false;
 	}
 }
