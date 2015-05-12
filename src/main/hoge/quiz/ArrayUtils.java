84766a71bf77a6ede061b9b3ad817113ee26c1f1
kiyotani masaki <makky0416@gmail.com>
2015-05-12 13:09:33 +0900

    値がnullの時に早期リターンするように修正しました。

--- src/main/hoge/quiz/ArrayUtils.java
+++ src/main/hoge/quiz/ArrayUtils.java
@@ -5,14 +5,24 @@ import java.util.List;
 public class ArrayUtils {
 
 	public static boolean equals(List<String> list1, List<String> list2){
-		if(list1 != null && list2 != null && list1.equals(list2)){
+
+		if(list1 == null || list2 == null){
+			return false;
+		}
+
+		if(list1.equals(list2)){
 			return true;
 		}
 		return false;
 	}
 
 	public static boolean contains(List<String> list1, List<String> list2){
-		if(list1 != null && list2 != null && list1.containsAll(list2)){
+
+		if(list1 == null || list2 == null){
+			return false;
+		}
+
+		if(list1.containsAll(list2)){
 			return true;
 		}
 		return false;
--- src/main/hoge/quiz/StringUtils.java
+++ src/main/hoge/quiz/StringUtils.java
@@ -6,16 +6,15 @@ import java.util.List;
 public class StringUtils {
 
 	public static List<String> distinct(List<String> list){
-		List<String> uniqueList = new ArrayList<String>();
-
 		if(list==null){
 			return list;
-		}else{
-			for(int i=0; i<list.size(); i++){
-				if(!uniqueList.contains(list.get(i))){
-					uniqueList.add(list.get(i));
-				}
-			}
+		}
+
+		List<String> uniqueList = new ArrayList<String>();
+
+		for(int i=0; i<list.size(); i++){
+			if(!uniqueList.contains(list.get(i))){
+				uniqueList.add(list.get(i));				}
 		}
 		return uniqueList;
 	}
