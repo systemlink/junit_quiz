03a90de5242f4135704e82a31a9a329d3ebb048b
kiyotani masaki <makky0416@gmail.com>
2015-05-12 10:59:57 +0900

    test4とtest5を実装しました。

--- src/main/hoge/quiz/ArrayUtils.java
+++ src/main/hoge/quiz/ArrayUtils.java
@@ -3,12 +3,18 @@ package hoge.quiz;
 import java.util.List;
 
 public class ArrayUtils {
-	
+
 	public static boolean equals(List<String> list1, List<String> list2){
+		if(list1 != null && list2 != null && list1.equals(list2)){
+			return true;
+		}
 		return false;
 	}
-	
+
 	public static boolean contains(List<String> list1, List<String> list2){
+		if(list1 != null && list2 != null && list1.containsAll(list2)){
+			return true;
+		}
 		return false;
 	}
 }
