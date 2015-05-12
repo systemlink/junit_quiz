94c22cca1fa06c982129fb3a88f5cbb57576a59a
yoshi836 <y-toya@sys-link.jp>
2015-05-12 09:48:49 +0900

    test3 重複処理を実装しました。

--- src/main/hoge/quiz/StringUtils.java
+++ src/main/hoge/quiz/StringUtils.java
@@ -1,11 +1,21 @@
 package hoge.quiz;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StringUtils {
 	public static List<String> distinct(List<String> list){
+		List<String> uniqueArray = new ArrayList<String>();
 
+		if(list == null){
+			return null;
+		}
 
-		return list;
+		for(String s : list){
+			if(!uniqueArray.contains(s)){
+				uniqueArray.add(s);
+			}
+		}
+		return  uniqueArray;
 	}
 }
