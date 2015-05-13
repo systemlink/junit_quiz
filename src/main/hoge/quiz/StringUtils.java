a9031ac200220d38b8e6281974e35fd44dc95f63
yoshi836 <y-toya@sys-link.jp>
2015-05-13 09:22:33 +0900

    test6 空文字、null判定を実装しました。

--- src/main/hoge/quiz/StringUtils.java
+++ src/main/hoge/quiz/StringUtils.java
@@ -17,4 +17,39 @@ public class StringUtils {
 		}
 		return  uniqueArray;
 	}
+
+    public static boolean isBlank(String str){
+    	if(str == null || str.length() == 0){
+    		return true;
+    	}
+        return false;
+    }
+
+    public static boolean indexOf(String str,char searchChar){
+        return false;
+    }
+
+    public static String defaultString(String str){
+        return null;
+    }
+
+    public static String defaultString(String str, String defaultStr){
+        return null;
+    }
+
+    public static int length(String str){
+        return (Integer) null;
+    }
+
+    public static String substring(String str, int start){
+        return null;
+    }
+
+    public static String substring(String str, int start ,int end){
+        return null;
+    }
+
+    public static String replace(String text, String searchString, String replacement){
+        return null;
+    }
 }
