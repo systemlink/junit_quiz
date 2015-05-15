638231d35d6f78fa3c33424a60fea7a7f2e7c08b
yoshi836 <y-toya@sys-link.jp>
2015-05-15 10:04:20 +0900

    test11 substring修正。

--- src/main/hoge/quiz/StringUtils.java
+++ src/main/hoge/quiz/StringUtils.java
@@ -4,98 +4,100 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StringUtils {
-	public static List<String> distinct(List<String> list){
-		if(list == null){
+	public static List<String> distinct(List<String> list) {
+		if (list == null) {
 			return null;
 		}
 
 		List<String> uniqueArray = new ArrayList<String>();
-		for(String s : list){
-			if(!uniqueArray.contains(s)){
+		for (String s : list) {
+			if (!uniqueArray.contains(s)) {
 				uniqueArray.add(s);
 			}
 		}
-		return  uniqueArray;
+		return uniqueArray;
 	}
 
-    public static boolean isBlank(String str){
-    	if(str == null || str.length() == 0){
-    		return true;
-    	}
-        return false;
-    }
+	public static boolean isBlank(String str) {
+		if (str == null || str.length() == 0) {
+			return true;
+		}
+		return false;
+	}
 
-    public static int indexOf(String str,char searchChar){
-    	if(str == null || str.length() == 0){
-    		return -1;
-    	}
+	public static int indexOf(String str, char searchChar) {
+		if (str == null || str.length() == 0) {
+			return -1;
+		}
 
-        return str.indexOf(searchChar);
-    }
+		return str.indexOf(searchChar);
+	}
 
-    public static String defaultString(String str){
-    	if(str == null)
-    		str = "";
-        return str;
-    }
+	public static String defaultString(String str) {
+		if (str == null)
+			str = "";
+		return str;
+	}
 
-    public static String defaultString(String str, String defaultStr){
-    	if (str == null){
-    		return defaultStr;
-    	}
-        return str;
-    }
+	public static String defaultString(String str, String defaultStr) {
+		if (str == null) {
+			return defaultStr;
+		}
+		return str;
+	}
 
-    public static int length(String str){
-    	if(str == null){
-    		return 0;
-    	}
+	public static int length(String str) {
+		if (str == null) {
+			return 0;
+		}
 
-        return str.length();
-    }
+		return str.length();
+	}
 
-    public static String substring(String str, int start){
-    	if(str == null || str == ""){
-    		return str;
-    	}
+	public static String substring(String str, int start) {
+		if (str == null) {
+			return null;
+		}
 
-    	if(str.length() < start){
-    		return "";
-    	}
+		if (str == "" || str.length() < start) {
+			return "";
+		}
 
-    	if(start < 0){
-    		if(str.length() < - start){
-    			return str;
-    		}
-    		return str.substring(str.length() + start);
-    	}
+		if (start < 0) {
+			if (str.length() > -start) {
+				start = str.length() + start;
+			} else {
+				start = 0;
+			}
+		}
 
-        return str.substring(start);
-    }
+		return str.substring(start);
+	}
 
-    public static String substring(String str, int start ,int end){
-       	if(str == null || str == ""){
-    		return str;
-    	}
+	public static String substring(String str, int start, int end) {
+		if (str == null || str == "") {
+			return str;
+		}
 
-       	if(end == 0 || str.length() <= start){
-       		return "";
-       	}
+		if (end == 0 || str.length() <= start) {
+			return "";
+		}
 
-	    if(str.length() <  end){
-	    	return str.substring(str.length() - 1);
-	    }
+		if (str.length() < end) {
+			return str.substring(str.length() - 1);
+		}
 
-       	if(start < 0){
-       		if(str.length() < - start){
-       			return str.substring(0,end);
-       		}
-       		return str.substring(str.length() + start , str.length() + end);
-       	}
-        return str.substring(start, end);
-    }
+		if (start < 0) {
+			if (str.length() < -start) {
+				return str.substring(0, end);
+			}
+			return str.substring(str.length() + start, str.length() + end);
+		}
+		return str.substring(start, end);
+	}
 
-    public static String replace(String text, String searchString, String replacement){
-        return null;
-    }
+	public static String replace(String text, String searchString,
+			String replacement) {
+		return null;
+	}
 }
