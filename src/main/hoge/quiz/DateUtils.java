f34bbeaaa214bd172a78b9361cc367a3cb274700
kohara <asigaru_1059@yahoo.co.jp>
2017-05-29 11:39:03 +0900

    test2 commit

--- src/main/hoge/quiz/DateUtils.java
+++ src/main/hoge/quiz/DateUtils.java
@@ -29,6 +29,32 @@ public class DateUtils {
 	}
 
 	public static String toDayOfWeekForJapaneseThrowsException(int week) throws IllegalArgumentException {
-		return null;
+		String week_s;
+		switch (week) {
+		case 1:
+			week_s = "日";
+			break;
+		case 2:
+			week_s = "月";
+			break;
+		case 3:
+			week_s = "火";
+			break;
+		case 4:
+			week_s = "水";
+			break;
+		case 5:
+			week_s = "木";
+			break;
+		case 6:
+			week_s = "金";
+			break;
+		case 7:
+			week_s = "土";
+			break;
+		default :
+			throw new IllegalArgumentException("引数に1-7の数字を指定してください。指定値=" + week);
+		}
+		return week_s;
 	}
 }
