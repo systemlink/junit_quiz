b412b78586f7d40606bdabcd4aad05ac29ed5044
matsumoto <r-matsumoto@sys-link.jp>
2017-05-29 10:15:05 +0900

    Test1 HashMap

--- src/main/hoge/quiz/DateUtils.java
+++ src/main/hoge/quiz/DateUtils.java
@@ -1,11 +1,34 @@
 package hoge.quiz;
 
+import java.util.HashMap;
+
 public class DateUtils {
-	
-	public static String toDayOfWeekForJapanese(int week){
+
+	public static String toDayOfWeekForJapanese(int week) {
 		// テストコミット
 		String week_d;
-		switch(week) {
+		HashMap<Integer, String> map = new HashMap<Integer, String>() {
+			{
+				put(1, "日");
+				put(2, "月");
+				put(3, "火");
+				put(4, "水");
+				put(5, "木");
+				put(6, "金");
+				put(7, "土");
+			}
+		};
+		if (true == map.containsKey(week)) {
+			week_d = map.get(week);
+		} else {
+			week_d = "";
+		}
+		return week_d;
+	}
+
+	public static String toDayOfWeekForJapaneseThrowsException(int week) throws IllegalArgumentException {
+		String week_d;
+		switch (week) {
 		case 1:
 			week_d = "日";
 			break;
@@ -27,14 +50,12 @@ public class DateUtils {
 		case 7:
 			week_d = "土";
 			break;
+		case 8:
+			throw new IllegalArgumentException("引数に1-7の数字を指定してください。指定値=8");
 		default:
 			week_d = "";
 			break;
 		}
 		return week_d;
-	}
-	
-	public static String toDayOfWeekForJapaneseThrowsException(int week) throws IllegalArgumentException{
-		return null;
 	}
 }
