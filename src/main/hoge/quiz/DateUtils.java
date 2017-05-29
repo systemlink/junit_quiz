1557800a23fa14f539288f9955b40b564b9da311
kohara <asigaru_1059@yahoo.co.jp>
2017-05-29 10:13:53 +0900

    test1 commit2

--- src/main/hoge/quiz/DateUtils.java
+++ src/main/hoge/quiz/DateUtils.java
@@ -1,34 +1,30 @@
 package hoge.quiz;
 
+import java.util.HashMap;
+
 public class DateUtils {
 
+	static HashMap<Integer, String> week_map = new HashMap<Integer, String>() {
+		{
+			put(1, "日");
+			put(2, "月");
+			put(3, "火");
+			put(4, "水");
+			put(5, "木");
+			put(6, "金");
+			put(7, "土");
+		}
+	};
+	
 	public static String toDayOfWeekForJapanese(int week) {
 		String week_s;
-		switch (week) {
-		case 1:
-			week_s = "日";
-			break;
-		case 2:
-			week_s = "月";
-			break;
-		case 3:
-			week_s = "火";
-			break;
-		case 4:
-			week_s = "水";
-			break;
-		case 5:
-			week_s = "木";
-			break;
-		case 6:
-			week_s = "金";
-			break;
-		case 7:
-			week_s = "土";
-			break;
-		default :
+		
+		if(week_map.containsKey(week)) {
+			week_s = week_map.get(week);
+		} else {
 			week_s = "";
 		}
+		
 		return week_s;
 	}
 
