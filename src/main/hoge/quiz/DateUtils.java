50e55d1ab4b816190e18a9baaad886501dadd1b4
matsumoto <r-matsumoto@sys-link.jp>
2017-05-30 09:24:05 +0900

    Test1 修正

--- src/main/hoge/quiz/DateUtils.java
+++ src/main/hoge/quiz/DateUtils.java
@@ -3,22 +3,22 @@ package hoge.quiz;
 import java.util.HashMap;
 
 public class DateUtils {
-
+	static HashMap<Integer, String> map = new HashMap<Integer, String>() {
+		{
+			put(1, "日");
+			put(2, "月");
+			put(3, "火");
+			put(4, "水");
+			put(5, "木");
+			put(6, "金");
+			put(7, "土");
+		}
+	};
 	public static String toDayOfWeekForJapanese(int week) {
 		// テストコミット
 		String week_d;
-		HashMap<Integer, String> map = new HashMap<Integer, String>() {
-			{
-				put(1, "日");
-				put(2, "月");
-				put(3, "火");
-				put(4, "水");
-				put(5, "木");
-				put(6, "金");
-				put(7, "土");
-			}
-		};
-		if (true == map.containsKey(week)) {
+		
+		if (map.containsKey(week)) {
 			week_d = map.get(week);
 		} else {
 			week_d = "";
@@ -28,33 +28,13 @@ public class DateUtils {
 
 	public static String toDayOfWeekForJapaneseThrowsException(int week) throws IllegalArgumentException {
 		String week_d;
-		switch (week) {
-		case 1:
-			week_d = "日";
-			break;
-		case 2:
-			week_d = "月";
-			break;
-		case 3:
-			week_d = "火";
-			break;
-		case 4:
-			week_d = "水";
-			break;
-		case 5:
-			week_d = "木";
-			break;
-		case 6:
-			week_d = "金";
-			break;
-		case 7:
-			week_d = "土";
-			break;
-		case 8:
+		
+		if (map.containsKey(week)) {
+			week_d = map.get(week);
+		} else if(week == 8) {
 			throw new IllegalArgumentException("引数に1-7の数字を指定してください。指定値=8");
-		default:
+		} else {
 			week_d = "";
-			break;
 		}
 		return week_d;
 	}
--- src/main/hoge/quiz/StringUtils.java
+++ src/main/hoge/quiz/StringUtils.java
@@ -1,9 +1,19 @@
 package hoge.quiz;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class StringUtils {
-	public static List<String> distinct(List<String> list){
-		return null;
+	public static List<String> distinct(List<String> list) {
+
+		if (list == null) {
+			return null;
+		} else {
+			Set<String> set = new LinkedHashSet<String>(list);
+			List<String> list2 = new ArrayList<String>(set);
+			return list2;
+		}
 	}
 }
