e901bcd48dc2251ba1a5e503ab23df800adc2148
matsumoto <r-matsumoto@sys-link.jp>
2017-06-01 11:25:15 +0900

    Test4 修正

--- src/main/hoge/quiz/ArrayUtils.java
+++ src/main/hoge/quiz/ArrayUtils.java
@@ -3,12 +3,27 @@ package hoge.quiz;
 import java.util.List;
 
 public class ArrayUtils {
-	
-	public static boolean equals(List<String> list1, List<String> list2){
-		return false;
+
+	public static boolean equals(List<String> list1, List<String> list2) {
+
+		if (list1 == null || list2 == null) {
+			return false;
+		} else if (list1.equals(list2)) {
+			return true;
+		} else {
+			return false;
+		}
+
 	}
-	
-	public static boolean contains(List<String> list1, List<String> list2){
-		return false;
+
+	public static boolean contains(List<String> list1, List<String> list2) {
+
+		if (list1 == null || list2 == null) { 
+			return false;
+		} else if(list1.contains(list2) || list1.size() < list2.size()){
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
