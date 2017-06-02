0cf035930d84184cf0276ee9ced13a28017a4f77
kohara <asigaru_1059@yahoo.co.jp>
2017-06-02 11:53:49 +0900

    test4 commit

--- src/main/hoge/quiz/ArrayUtils.java
+++ src/main/hoge/quiz/ArrayUtils.java
@@ -3,12 +3,20 @@ package hoge.quiz;
 import java.util.List;
 
 public class ArrayUtils {
-	
-	public static boolean equals(List<String> list1, List<String> list2){
-		return false;
+
+	public static boolean equals(List<String> list1, List<String> list2) {
+		if (list1 == null || list2 == null) {
+			return false;
+		}
+		
+		return list1.equals(list2);
 	}
-	
-	public static boolean contains(List<String> list1, List<String> list2){
-		return false;
+
+	public static boolean contains(List<String> list1, List<String> list2) {
+		if (list1 == null || list2 == null) {
+			return false;
+		}
+
+		return list1.containsAll(list2);
 	}
 }
