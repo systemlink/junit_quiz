05d453d6dfae365cb3c285dc66760f793d25a1da
yuyamatsymura <shousen.0917@gmail.com>
2019-10-11 11:06:38 +0900

    test3追加しました

--- src/main/hoge/quiz/StringUtils.java
+++ src/main/hoge/quiz/StringUtils.java
@@ -1,9 +1,14 @@
 package hoge.quiz;
 
+
+import java.util.stream.Collectors;
 import java.util.List;
 
 public class StringUtils {
 	public static List<String> distinct(List<String> list){
-		return null;
+		if(list == null) {
+			return null;
+		}
+		return list.stream().distinct().collect(Collectors.toList());
 	}
 }
