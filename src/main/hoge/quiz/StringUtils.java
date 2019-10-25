5cd591a8996ebfe5feba958c6ff8e849fff64528
yuyamatsymura <shousen.0917@gmail.com>
2019-10-25 17:52:39 +0900

    test6-test10まで修正し、test11追加しました。

--- src/main/hoge/quiz/StringUtils.java
+++ src/main/hoge/quiz/StringUtils.java
@@ -1,8 +1,11 @@
 package hoge.quiz;
 
-
 import java.util.stream.Collectors;
+import java.util.Collections;
 import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+
 
 public class StringUtils {
 	public static List<String> distinct(List<String> list){
@@ -13,30 +16,39 @@ public class StringUtils {
 	}
 	
 	public static boolean isBlank(String str) {
-		if(str == null || str == "" || str == " " || str == "Å@" ) {
+		if(str == null || str == "") {
 			return true;
 		}
+		List<String> strList = Arrays.asList(str.split(""));
+		if(Collections.frequency(strList,strList.get(0)) == strList.size()) {
+			if(strList.get(0) == " " || strList.get(0) == "Å@") {
+				return true;
+			}
+		}
 		return false;
-	
+		
+//		if(org.apache.commons.lang3.StringUtils.isBlank(str)) {
+//			return true;
+//		}
+//		return false;
 	}
 
 	public static int indexOf(String str, char searchChar) {
 		if(str == null || str == "" ) {
 			return -1;
 		}
-//		char[] chrArray = str.toCharArray();
-//		for(int i = 0;i < chrArray.length;i++) {
-//			if(chrArray[i] == searchChar) {
-//				return i;
-//			}
-//		}
-//		return -1;
-		return str.indexOf(searchChar);
+		char[] chrArray = str.toCharArray();
+		for(int i = 0;i < chrArray.length;i++) {
+			if(chrArray[i] == searchChar) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	public static String defaultString(String str) {
 		if(str == null) {
-			return str = "";
+			return "";
 		}
 		return str;
 	}
@@ -49,22 +61,43 @@ public class StringUtils {
 	}
 
 	public static int length(String str) {
-		if(str == null || str.length() == 0) {
+		int count = 0;
+		if(str == null || str == "") {
 			return 0;
 		}
-		return str.length();
+		String[] strArray = str.split("");
+		for(int i = 0;i < strArray.length;i++) {
+			count += 1;
+		}
+		return count;
 	}
 
 	public static String substring(String str, int start) { 
-//		if(str == null) {
-//			return null;
-//		}
-//		
-//		if(str == "") {
-//			return "";
-//		}
-		return str.substring(start);
+		if(str == null) {
+			return null;
+		}else if(str == "" || str.length() -start < 0) {
+			return "";
+		}
 		
+		String[] strArray = str.split("");
+		List<String> strList = new ArrayList<String>();
+		if(start < 0) {
+			if(str.length() + start < 0) {
+				for(int i = 0;i < str.length();i++) {
+					strList.add(strArray[i]);
+				}
+			}else {
+				for(int i = str.length() + start; i < str.length();i++) {
+					strList.add(strArray[i]);
+				}
+			}
+		}else {
+			for (int i = start;i < str.length();i++) {
+				strList.add(strArray[i]);
+			}
+		}
+		
+		return str = String.join("", strList);
 	}
 
 	public static String substring(String str, int start, int end) {
